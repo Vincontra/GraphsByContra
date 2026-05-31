c1eb9f5ca8b8bacd881d3a096886cc2ac89f3b6c
vinay <vinaygadge2003@gmail.com>
2026-05-31 14:31:09 +0530

    BFS added in this code

--- NoOfIslands.java
+++ NoOfIslands.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class NoOfIslands {
     public int numIslands(char[][]arr) {
             int m=arr.length;
@@ -8,6 +9,7 @@ public class NoOfIslands {
                     if(arr[i][j]=='1'){
                         cnt++;
                         dfs(i,j,arr,m,n);
+                        //bfs(i,j,arr,m,n);
                     }
                 }
             }
@@ -25,5 +27,36 @@ public class NoOfIslands {
             dfs(i+1,j,arr,m,n);
             dfs(i,j-1,arr,m,n);
             dfs(i,j+1,arr,m,n);
+    }
+    public static void bfs(int i, int j, char[][] arr, int m, int n) {
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{i, j});
+        arr[i][j]='0'; // visited
+        while (!q.isEmpty()) {
+            int[] curr = q.remove();
+            int r = curr[0];
+            int c = curr[1];
+            // Up
+            if (r - 1 >= 0 && arr[r - 1][c] == '1') {
+                arr[r - 1][c] = '0';
+                q.add(new int[]{r - 1, c});
+            }
+            // Down
+            if (r + 1 < m && arr[r + 1][c] == '1') {
+                arr[r + 1][c] = '0';
+                q.add(new int[]{r + 1, c});
+            }
+            // Left
+            if (c - 1 >= 0 && arr[r][c - 1] == '1') {
+                arr[r][c - 1] = '0';
+                q.add(new int[]{r, c - 1});
+            }
+            // Right
+            if (c + 1 < n && arr[r][c + 1] == '1') {
+                arr[r][c + 1] = '0';
+                q.add(new int[]{r, c + 1});
+            }
+        }
+
     }
 }
