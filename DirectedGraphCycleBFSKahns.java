38bd86a3cddf4d1d52dd96ff904b4bf29a8332d1
vinay <vinaygadge2003@gmail.com>
2026-06-03 14:56:31 +0530

    I used Kahns Algo To detect cycle in Directed Graph;Question is on GFG

---
+++ DirectedGraphCycleBFSKahns.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DirectedGraphCycleBFSKahns {
+    public boolean isCyclic(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+        int indegree[]=new int[V];
+        for (int i=0;i<V;i++){
+            for (int j:adj.get(i)){
+                indegree[j]++;
+            }
+        }
+        Queue<Integer>q1=new LinkedList<>();
+        for (int i=0;i<indegree.length;i++){
+            if (indegree[i]==0){
+                q1.add(i);
+            }
+        }
+        ArrayList<Integer>l1=new ArrayList<>();
+        while (!q1.isEmpty()){
+            int curr=q1.remove();
+            l1.add(curr);
+            for (int i:adj.get(curr)){
+                indegree[i]--;
+                if (indegree[i]==0){
+                    q1.add(i);
+                }
+            }
+        }
+        return l1.size()!=V;
+
+    }
+}
