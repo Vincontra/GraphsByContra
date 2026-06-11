21f5095d7736c8104a0eec18daca2d5b64e134b6
vinay <vinaygadge2003@gmail.com>
2026-06-11 13:05:01 +0530

    DisJointSetByRank and By size

--- DisJointSetUnion.java
+++ DisJointSetUnion.java
@@ -14,10 +14,13 @@ public class DisJointSetUnion
 class DisJoint{
     List<Integer>rank=new ArrayList<>();
     List<Integer>par=new ArrayList<>();
+
+    List<Integer>size=new ArrayList<>();
     public DisJoint(int n){
         for (int i=0;i<n;i++){
             rank.add(0);
             par.add(i);
+            size.add(1);
         }
     }
     public  int findUPar(int node){
@@ -35,10 +38,10 @@ class DisJoint{
         if (ulp_u==ulp_v){
             return;
         }
-        if (ulp_u<ulp_v){
+        if (rank.get(ulp_u)<rank.get(ulp_v)){
             par.set(ulp_u,ulp_v);
         }
-        else if (ulp_u>ulp_v){
+        else if (rank.get(ulp_u)>rank.get(ulp_v)){
             par.set(ulp_v,ulp_u);
         }
         else{
@@ -46,6 +49,22 @@ class DisJoint{
             par.set(ulp_u,ulp_v);
             rank.set(ulp_v,rank.get(ulp_v)+1);
         }
+    }
+    public void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if (ulp_u==ulp_v){
+            return;
+        }
+        if (size.get(ulp_u)<size.get(ulp_v)){
+            par.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
+        else{
+            par.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }
+
     }
 
 }
